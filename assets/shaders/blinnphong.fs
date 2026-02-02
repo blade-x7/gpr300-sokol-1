f9b32c77aaa30e834f865baed590229259542c24
blade-x7 <portia.flaherty@icloud.com>
2026-02-02 16:59:23 -0500

    we got suzanne back, material struct implemented and functional

--- assets/shaders/blinnphong.fs
+++ assets/shaders/blinnphong.fs
@@ -10,11 +10,11 @@ struct Light{
 };
 
 struct Material{
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    vec3 shininess;
-}
+    float ambient;
+    float diffuse;
+    float specular;
+    float shininess;
+};
 
 // varyings
 in vec3 vs_position;
@@ -30,15 +30,14 @@ vec3 blinnphong(vec3 normal, vec3 frag_pos, Light light) {
     // glsl: dot(vec3, vec3)
     vec3 view_dir = normalize(camera - frag_pos);
     vec3 light_dir = normalize(light.position - frag_pos);
-    vec3 reflect_dir = reflect(light_dir, vs_normal);
+    vec3 reflect_dir = reflect(light_dir, normal);
     vec3 half_dir = normalize(light_dir + view_dir);
 
     //apply material
-    float NdotL = max(dot(vs_normal, light_dir), 0.0);
+    float NdotL = max(dot(normal, light_dir), 0.0);
     //shininess
-    float NdotH = pow(max(dot(vs_normal, half_dir), 0.0), material.shininess);
-
-    vec3 diffuse = NdotL * material.diffuse;
+    float NdotH = pow(max(dot(normal, half_dir), 0.0), material.shininess);
+    
     
 
     vec3 finalBP = vec3(NdotL + NdotH);
@@ -48,7 +47,7 @@ vec3 blinnphong(vec3 normal, vec3 frag_pos, Light light) {
 void main()
 {
     vec3 ambient = vec3(1.0);
-    vec3 lighting = blinnphong(vs_normal, vs_position, light.xyz) + ambient * material.ambient;
+    vec3 lighting = blinnphong(vs_normal, vs_position, light) + material.ambient;
     vec3 object_color = vs_normal * 0.5 + 0.5;
     vec3 final_color = object_color * lighting;
     FragColor = vec4(final_color, 1.0);
